c8832796a4e2a5b250a0a47d3671cfbb2654b7e4
chunhunghan <chunhunghan@gmail.com>
2019-07-25 15:57:04 +0800

    rebase

--- desktop/cmd/options.go
+++ desktop/cmd/options.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"github.com/go-flutter-desktop/go-flutter"
+	"github.com/chunhunghan/file_picker"
 )
 
 var options = []flutter.Option{
 	flutter.WindowInitialDimensions(800, 1280),
+	flutter.AddPlugin(&file_picker.FilePickerPlugin{}),
 }
